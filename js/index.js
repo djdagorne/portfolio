3825971c0b50b9b2c5c4991d39298faed139a8c2
Dexter Dagorne <dj.dagorne@gmail.com>
2020-06-26 21:32:36 -0400

    parallax on dex pics

--- js/index.js
+++ js/index.js
@@ -10,3 +10,15 @@ navLinks.forEach((link) => {
     document.body.classList.remove("nav-open");
   });
 });
+
+const intro = document.getElementById("intro");
+const pic2 = document.getElementsByClassName("about-me__img")[0];
+
+window.addEventListener("scroll", function () {
+  /* hero div parallax */
+  const offset = window.pageYOffset;
+  intro.style.top = offset * 0.75 + "px";
+  /* slight parallax on second img */
+  const offset2 = window.pageYOffset / 7;
+  pic2.style.top = offset2 * 0.5 + "px";
+});
