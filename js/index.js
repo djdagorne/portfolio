8d3d03f40dcbdee7f9d748670055753d1a010f2d
Dexter Dagorne <dj.dagorne@gmail.com>
2020-07-02 06:14:10 -0400

    parrallax fix

--- js/index.js
+++ js/index.js
@@ -17,7 +17,8 @@ const pic2 = document.getElementsByClassName("about-me__img")[0];
 window.addEventListener("scroll", function () {
   /* hero div parallax */
   const offset = window.pageYOffset;
-  intro.style.top = offset * 0.75 + "px";
+  intro.style.top = offset * 0.9 + "px";
+
   /* slight parallax on second img */
   const offset2 = window.pageYOffset / 7;
   pic2.style.top = offset2 * 0.5 + "px";
