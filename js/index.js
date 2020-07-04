c273ad801caa530a8719db71b6302eef50a1933e
Dexter Dagorne <dj.dagorne@gmail.com>
2020-07-03 21:39:55 -0400

    hero fix

--- js/index.js
+++ js/index.js
@@ -11,13 +11,13 @@ navLinks.forEach((link) => {
   });
 });
 
-const intro = document.getElementById("intro");
+const hero = document.getElementById("hero");
 const pic2 = document.getElementsByClassName("about-me__img")[0];
 
 window.addEventListener("scroll", function () {
   /* hero div parallax */
   const offset = window.pageYOffset;
-  intro.style.top = offset * 0.9 + "px";
+  hero.style.top = offset * 0.9 + "px";
 
   /* slight parallax on second img */
   const offset2 = window.pageYOffset / 7;
